ecb6adfe53f380389f4d5f113194e4648a5b45e6
Dominic Ross <youngdad33@gmail.com>
2023-06-14 08:45:37 +0100

    Got the background colour of the Divs to show from the JS

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const colourArray = ["#F55A5A", "#2B283A", "#FBF3AB", "#AAD1B6", "#A626D3"]
 const colourSection = document.getElementById("colour-section")
 
+
+
 for (let colour of colourArray){
   colourSection.innerHTML += `
-    <div class="returned-colour" id="${colour}"></div>
+    <div class="returned-colour" id="${colour}">
     <footer>${colour}</footer>
-  ` 
+    </div>
+    
+  `
+  document.getElementById(colour).style.backgroundColor = colour
+  console.log(colour)
 }
