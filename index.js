5bfe7249955c6bc3fe1b5076b8ffd7fa53b3b69e
Dominic Ross <youngdad33@gmail.com>
2023-06-14 08:17:14 +0100

    Rendered an array of colours to the dom

---
+++ index.js
@@ -0,0 +1,9 @@
+const colourArray = ["#F55A5A", "#2B283A", "#FBF3AB", "#AAD1B6", "#A626D3"]
+const colourSection = document.getElementById("colour-section")
+
+for (let colour of colourArray){
+  colourSection.innerHTML += `
+    <div class="returned-colour" id="${colour}"></div>
+    <footer>${colour}</footer>
+  ` 
+}
