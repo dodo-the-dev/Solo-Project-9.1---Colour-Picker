71875e26cbbb7e1b96f76a08b9decb4b9c671bc4
Dominic Ross <youngdad33@gmail.com>
2023-06-15 13:36:54 +0100

    retrieved value of hex and displayed to DOM

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const colourArray = ["#F55A5A", "#2B283A", "#FBF3AB", "#AAD1B6", "#A626D3"]
 const colourSection = document.getElementById("colour-section")
-// const seedColour = 
-// const colourScheme = 
+const fetchColourScheme = document.getElementById("fetch-colour-scheme")
 let seedValue = ""
 let schemeValue = ""
 
 
-for (let colour of colourArray){
-  colourSection.innerHTML += `
-    <div class="returned-colour" id="${colour}">
-    <footer>${colour}</footer>
-    </div>
-    
-  `
-  document.getElementById(colour).style.backgroundColor = colour
+
+
+function getSeedColour(){
+  return document.getElementById("seed-colour").value.slice(1)
 }
 
-function getSeedColour(seedValue){
-  seedValue =  document.getElementById("seed-colour").value
-  return seedValue
+
+function getColourScheme(){
+  return document.getElementById("colour-scheme").value  
 }
 
-function getColourScheme(schemeValue){
-  schemeValue = document.getElementById("colour-scheme").value
-  return schemeValue
-}
-
-  // seedValue = seedColour.addEventListener("input", ()=>  {return seedColour.value})
-
-
-  // schemeValue = colourScheme.addEventListener("input", () => {return colourScheme.value})
-
-  console.log(seedValue)
+fetchColourScheme.addEventListener("click", function(){
+  seedValue = getSeedColour()
+  schemeValue = getColourScheme()
+  let url = "https://www.thecolorapi.com/scheme?hex="+seedValue+"&mode="+schemeValue
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      colourSection.innerHTML =""
+      for (let colour of data.colors){
+        colourSection.innerHTML +=`
+          <div class="returned-colour" id="${colour.hex.value}">
+          <footer>${colour.hex.value}</footer>
+          </div>
+          
+        `
+        document.getElementById(colour.hex.value).style.backgroundColor = colour.hex.value
+      }
+    })
+})
